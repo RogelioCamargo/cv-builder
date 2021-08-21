8baf6b98815dca3edf747b670103168343b62abf
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-08-21 15:52:42 -0700

    Create a preview where user will see their inputs in real time

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,57 @@
 import React, { Component } from "react";
 
+import Education from "./Education";
+import Experience from "./Experience";
+import General from "./General";
+import Preview from "./Preview";
+
 class Main extends Component {
   constructor() {
     super();
 
     this.state = {
-      general: {
-        firstName: "",
+      personal: {
+        firstName: "Rogelio",
+        lastName: "Camargo",
+        email: "rogelio.camargo2016@gmail.com",
+        phoneNumber: "(408)981-1510",
       },
-      education: [
-        {
-          school: "",
-        }
-      ], 
       experience: [
         {
-          company: "",
-        }
-      ]
-    }
+          company: "Ohi Technolgies",
+          position: "Software Engineer",
+          from: "2020",
+          to: "Present",
+        },
+      ],
+      education: [
+        {
+          university: "University of California, San Diego",
+          major: "Computer Science",
+          from: "2018",
+          to: "2020",
+        },
+      ],
+    };
   }
 
+  
+
   render() {
-    return <></>;
+    const { personal, experience, education } = this.state;
+
+    return (
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "50%" }}>
+          <General info={personal} />
+          <Experience info={experience} />
+          <Education info={education} />
+        </div>
+        <div style={{ width: "50%" }}>
+          <Preview info={this.state} />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -7,7 +7,44 @@ class Preview extends Component {
   }
 
   render() {
-    return <></>;
+    const { personal, experience, education } = this.props.info;
+    return (
+      <div>
+        <div>
+          <h2>Personal</h2>
+          <p>{personal.firstName}</p>
+          <p>{personal.lastName}</p>
+          <p>{personal.email}</p>
+          <p>{personal.phoneNumber}</p>
+        </div>
+        <div>
+          <h2>Experience</h2>
+          {experience.map((item) => {
+            return (
+              <div>
+                <p>{item.company}</p>
+                <p>{item.position}</p>
+                <p>{item.from}</p>
+                <p>{item.to}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <h2>Education</h2>
+          {education.map((item) => {
+            return (
+              <div>
+                <p>{item.university}</p>
+                <p>{item.major}</p>
+                <p>{item.from}</p>
+                <p>{item.to}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
